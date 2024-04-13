f5a25f61d5cdb80052f9f3a4b383ffe7f4b8efea
AshutoshJena2497 <ashutoshjena8215@gmail.com>
2024-04-13 14:57:20 +0530

    add the nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NabBarComponent } from './nab-bar/nab-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
+  imports: [RouterOutlet,HomeComponent,NabBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
