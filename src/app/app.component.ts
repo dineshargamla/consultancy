aff4afab73c17234b03e8cd4ac69bd979156151e
AshutoshJena2497 <ashutoshjena8215@gmail.com>
2024-04-13 15:01:26 +0530

    fix the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NabBarComponent } from './nab-bar/nab-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,NabBarComponent],
+  imports: [RouterOutlet,HomeComponent,NabBarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
