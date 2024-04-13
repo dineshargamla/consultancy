d0f543e20297b0925f15e502e9d7fb8f15cf7177
AshutoshJena2497 <ashutoshjena8215@gmail.com>
2024-04-13 15:40:23 +0530

    feat: 🎸 add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ImmigrationComponent } from './immigration/immigration.component';
+import { VisaComponent } from './visa/visa.component';
+import { StudyComponent } from './study/study.component';
+import { VisaAssessmentComponent } from './visa-assessment/visa-assessment.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HomeComponent},
+    {path:'about', component: AboutComponent},
+    {path:'contact', component: ContactComponent},
+    {path:'immigration', component: ImmigrationComponent},
+    {path:'visa', component: VisaComponent},
+    {path:'study', component: StudyComponent},
+    { path: 'vise-assessment', component: VisaAssessmentComponent },
+    {path: '**', component: PageNotFoundComponent}
+];
